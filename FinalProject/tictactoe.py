bd8d8498e3f0e03e888640da0194426d3136ae97
Faiz Hilaly <faiz@hilaly.com>
2022-05-12 23:31:25 -0700

    This is the fully working game and algorithm with commented code.

--- FinalProject/tictactoe.py
+++ FinalProject/tictactoe.py
@@ -1,8 +1,19 @@
-import random
-import time
+"""
+Final Project
+Name: Faiz Hilaly
+Class: AT Computer Science
+Block: F
+Date: May 11 2022
 
+This is a strategy game played by two players on a 7 by 7 grid. 
+Player A and Player B each take turns placing down an X or an O respectively, 
+with a player winning when they have connected 5 pieces in a row. 
+On any given turn a player can also choose to change the position 
+of two of their existing pieces instead of placing a new piece.
+"""
 class TicTacToe:
   def __init__(self):
+    #create board as a 2D list
     self.board = [[0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0],
@@ -10,27 +21,31 @@ class TicTacToe:
                   [0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0],
                   [0,0,0,0,0,0,0]]
+    
+    #creates a list of all possible combinations of 5 in a row
     self.five_in_a_row = []
-        #check rows
+    #5 in a row
     for row in range(0,7):
       for col in range(0,3):
         self.five_in_a_row.append([row,col,row,col+1,row,col+2,row,col+3,row,col+4])
-    #check columns
+    #5 in a column
     for row in range(0,3):
       for col in range(0,7):
           self.five_in_a_row.append([row,col,row+1,col,row+2,col,row+3,col,row+4,col])
-    #check diagonals
+    #5 in a diagonal
     for row in range(0,3):
       for col in range(0,3):
           self.five_in_a_row.append([row,col,row+1,col+1,row+2,col+2,row+3,col+3,row+4,col+4])
           self.five_in_a_row.append([6-row,col,5-row,col+1,4-row,col+2,3-row,col+3,2-row,col+4])
 
   @staticmethod
+  #print instructions
   def print_instructions():
     print("This game is played between two players, where players alternate taking turns.")
     print("On any given turn, a player may choose to either (1)place a new piece or (2)move two pieces to new positions on the board")
     print("The first player to connect 5 pieces in a row is the winner.")
 
+  #print the board
   def print_board(self):
     print("  0 1 2 3 4 5 6")
     for i in range(0,7):
@@ -58,6 +73,7 @@ class TicTacToe:
   def remove_player(self, player, row, col):
     self.board[row][col] = 0
 
+  #moves two existing pieces to two new positions based on coordinates input by user 
   def move_piece(self,player):
     old_row = int(input("Please enter the row number of the piece you would like to move: "))
     old_col = int(input("Please enter the column number of the piece you would like to move: "))
@@ -74,26 +90,21 @@ class TicTacToe:
     self.remove_player(player, old_row, old_col)
     self.place_player(player, new_row, new_col)
 
-
+  #user takes turn
   def take_manual_turn(self, player):
+    #Get input from user
     row = int(input("Please enter a row number between 0 and 6: "))
     col = int(input("Please enter a column number between 0 and 6: "))
 
+    #check that move is valid
     while not self.is_valid_move(row,col):
       row = int(input("Please enter a valid row number: "))
       col = int(input("Please enter a valid column number: "))
 
     self.place_player(player,row,col)
 
+  #algirthm takes turn
   def take_algorithm_turn(self,player):
-    #make it so that human can make a move 
-    #then computer makes a move based on which ever gets the highest score
-    #get every possible two pieces it has, then once you have them remove them and place them in 
-    # a new place of every possible two places
-    #generate a list of every space on the board where the alogirthm has pieces
-    #generate a list of every empty space on the board
-    #generate list of every pair of spaces and a list of every pair of pieces the alogirthm has
-    #consider the board where you remoe those two pieces and plcae it in those two spaces
     best_pieces_removed = []
     best_pieces_added = []
     best_score = 1000
@@ -121,8 +132,6 @@ class TicTacToe:
       for b in range(a+1,len(empty_spaces)):
         empty_paired_pieces.append([empty_spaces[a],empty_spaces[b]])
 
-    #empty_paired_pieces: [[0,0],[0,1]],[[0,0],[0,2]]
-    #pieces removed: [0,0],[0,1]]
     #check the best possible score that can be obtained by moving two pieces
     for a in range(0,len(alg_paired_pieces)):
       for b in range(0,len(empty_paired_pieces)):
@@ -145,6 +154,7 @@ class TicTacToe:
         self.board[pieces_added[0][0]][pieces_added[0][1]] = 0
         self.board[pieces_added[1][0]][pieces_added[1][1]] = 0
 
+    #check the best possible score that can be obtained by placing a new piece
     for row in range(0,7):
       for col in range(0,7):
         if self.is_valid_move(row,col):
@@ -162,25 +172,29 @@ class TicTacToe:
     for b in best_pieces_removed:
       self.remove_player(player,b[0],b[1])
 
-
+  #execute function for each player to take turn 
   def take_turn(self, player):
+    #user is player one and takes a manual turn (determined by user input).
     if player == 1:
       print("It is player " + str(player) + "'s turn")
       self.take_manual_turn(player)
+    #algorithm is player 2 (player -1). Move is based on algorithm function.
     if player == -1:
       print("It is player " + str(player*-2) + "'s turn")
       self.take_algorithm_turn(player)
 
-
+  #assigns each possible future board a number value based on how optimal the next move is
   def board_score(self):
     #Lower score means player 1 is better, higher score means player 2 is better
     score = 0
     b = self.board
 
+    #loop through every possible of 5 in a row
     for group_of_five_squares in self.five_in_a_row:
       num_pOne_appearances = 0
       num_pTwo_appearances = 0
 
+      #label each int in the list to what it represents
       row1 = group_of_five_squares[0]
       col1 = group_of_five_squares[1]
       row2 = group_of_five_squares[2]
@@ -192,20 +206,22 @@ class TicTacToe:
       row5 = group_of_five_squares[8]
       col5 = group_of_five_squares[9]
 
+      #check how many spaces in the list are occupied by Player 1
       if b[row1][col1] == 1: num_pOne_appearances += 1
       if b[row2][col2] == 1: num_pOne_appearances += 1
       if b[row3][col3] == 1: num_pOne_appearances += 1
       if b[row4][col4] == 1: num_pOne_appearances += 1
       if b[row5][col5] == 1: num_pOne_appearances += 1
 
+      #check how many spaces in the list are occupied by Player 2
       if b[row1][col1] == -1: num_pTwo_appearances += 1
       if b[row2][col2] == -1: num_pTwo_appearances += 1
       if b[row3][col3] == -1: num_pTwo_appearances += 1
       if b[row4][col4] == -1: num_pTwo_appearances += 1
       if b[row5][col5] == -1: num_pTwo_appearances += 1
 
+      #create a point value for possible situations
       winning_score = 1000000000
-
       alg_unblocked_four_score = 500
       player_unblocked_four_score = 600
       alg_unblocked_three_score = 300
@@ -215,6 +231,7 @@ class TicTacToe:
       alg_unblocked_one_score = 50
       player_unblocked_one_score = 75
 
+      #Assign the board a score based on the board situation
       if num_pOne_appearances == 5 and num_pTwo_appearances == 0: score += winning_score
       if num_pOne_appearances == 0 and num_pTwo_appearances == 5: score -= winning_score
       if num_pOne_appearances == 4 and num_pTwo_appearances == 0: score += player_unblocked_four_score
@@ -229,249 +246,79 @@ class TicTacToe:
     return score
 
 
-
+  #check if there is a winner
   def check_win(self, player):
     b = self.board
+    #takes in 10 ints and checks if a player has connected 5 in a row
     def check_five(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5):
       return b[x1][y1] == player and b[x2][y2] == player and b[x3][y3] == player and b[x4][y4] == player and b[x5][y5] == player
+    #check for 5 in a row
     for row in range(0,7):
       if check_five(row,0,row,1,row,2,row,3,row,4): return True
       if check_five(row,1,row,2,row,3,row,4,row,5): return True
       if check_five(row,2,row,3,row,4,row,5,row,6): return True
+    #check for 5 in a column
     for col in range(0,7):
       if check_five(0,col,1,col,2,col,3,col,4,col): return True
       if check_five(1,col,2,col,3,col,4,col,5,col): return True
       if check_five(2,col,3,col,4,col,5,col,6,col): return True
-
+    #check for 5 in a diagonal
     for row in range(0,3):
       for col in range(0,3):
         if check_five(row,col,row+1,col+1,row+2,col+2,row+3,col+3,row+4,col+4): return True
         if check_five(6-row,col,5-row,col+1,4-row,col+2,3-row,col+3,2-row,col+4): return True
     return False
 
+  #check if all empty spaces are occupied
   def check_tie(self):
     for i in range(0,7):
       if 0 in self.board[i]: return False
     return not self.check_win(1) and not self.check_win(-1)
 
+  #plays the game
   def play_game(self):
     c = 0
     TicTacToe.print_instructions()
     player = 1
     while True:
       self.print_board()
+      #check if both players have played more than two moves
       if(c >= 2):
-        if player == 1:
+        #give the person the option to either place a new piece or move two existing pieces to new positions
+        if player == 1: 
           turn_type = int(input("Would you like to (1)place a new piece or (2)move existing pieces."+
                                 "Please type the number that corresponds with your choice: "))
+          #makes sure a valid turn type is selected
           while not (turn_type == 1 or turn_type == 2):
             turn_type = int(input("Please select your choice by typing 1 or 2: "))
+            #user selects to place a new piece
           if turn_type == 1:
             self.take_turn(player)
+          #user selects to move two existing pieces
           if turn_type == 2:
             for i in range(0,2):
               print("Moving piece", str(i+1))
               self.move_piece(player)
+        #algorithm takes turn
         if player == -1:
           self.take_turn(player)         
+      #if both players have taken less than two turns, neither player gets the option to move two existing pieces
       else:
         self.take_turn(player)
 
+      #If a player wins, print out who and end the game
       if self.check_win(player):
         if player == 1: print("Player " + str(player) + " wins.")
         if player == -1: print("Player " + str(player*-2) + " wins.")
         self.print_board()
         break
+      #If a it's a tie, print that out and end the game
       if self.check_tie():
         print("It's a tie.")
         self.print_board()
         break
-      c=c+0.5
-      player = -1*player
-
-
-
-# def take_random_turn(self, player):
-#     row = random.randint(0,2)
-#     col = random.randint(0,2)
-    
-#     while not self.is_valid_move(row,col):
-#       row = random.randint(0,2)
-#       col = random.randint(0,2)
       
-#     return self.place_player(player,row,col)
-
-# def minimax_alpha_beta(self, player, depth, alpha, beta):
-#   #alpha = the lowest possible score (assuming player -1 is perfect)
-#   #beta = highest possible score assuming player 1 is perect
-#   #if alpha > beta, we stop searching 
-#   # Base case: Player 1 (X) tries to minimize, player -1 (O) maximizes
-#   if self.check_win(-1): return 10 + depth, None, None
-#   if self.check_win(1): return -10 - depth, None, None
-#   if self.check_tie() or depth == 0: return 0, None, None
-
-#   bestscore, row, col = player*20, None, None
-#   for i in [0,1,2]:
-#     for j in [0,1,2]:
-#       if alpha > beta: break
-#       if self.is_valid_move(i, j):
-#         self.place_player(player, i, j)
-#         score, r, c = self.minimax_alpha_beta(-1*player, depth - 1, alpha, beta)
-#         #player 1 is the minimizing player
-#         if player == 1:
-#           if score < beta: beta = score
-#         if player == -1:
-#           if score > alpha: alpha = score
-#         if player*(score - bestscore) < 0:
-#           bestscore, row, col = score, i, j
-#         self.place_player(0, i, j)
-#   return bestscore, row, col
-
-# #before ab pruning, this took 3.1951401233673096 for a blank board
-#   def take_minimax_turn(self, player):
-#     start = time.time()
-#     #score, row, col = self.minimax(player, 9999)
-#     score, row, col = self.minimax_alpha_beta(player, 9999, -30, 30)
-
-#     end = time.time()
-#     self.place_player(player, row, col)
-#     print("This turn took:", end - start, "seconds")
-
-#   def minimax(self, player, depth):
-#     # Base case: Player 1 (X) tries to minimize, player -1 (O) maximizes
-#     if self.check_win(-1): return 10 + depth, None, None
-#     if self.check_win(1): return -10 - depth, None, None
-#     if self.check_tie() or depth == 0: return 0, None, None
-
-#     bestscore, row, col = player*20, None, None
-#     for i in [0,1,2]:
-#       for j in [0,1,2]:
-#         if self.is_valid_move(i, j):
-#           self.place_player(player, i, j)
-#           score, r, c = self.minimax(-1*player, depth - 1)
-#           if player*(score - bestscore) < 0:
-#             bestscore, row, col = score, i, j
-#           self.place_player(0, i, j)
-#     return bestscore, row, col
-
-
-
-# def check_threat(self,player)
-  #     b = self.board
-  #   def check_three(x1,y1,x2,y2,x3,y3):
-  #     return b[x1][y1] == player and b[x2][y2] == player and b[x3][y3] == player
-  #   for row in range(0,7):
-  #     if check_three(row,0,row,1,row,2): return 1
-  #     if check_three(row,1,row,2,row,3): return 2
-  #     if check_three(row,2,row,3,row,4): return 2
-  #     if check_three(row,3,row,4,row,5): return 2
-  #     if check_three(row,4,row,5,row,6): return 3
-  #   for col in range(0,7):
-  #     if check_three(0,col,1,col,2,col): return 4
-  #     if check_three(1,col,2,col,3,col): return 5
-  #     if check_three(2,col,3,col,4,col): return 5
-  #     if check_three(3,col,4,col,5,col): return 5
-  #     if check_three(4,col,5,col,6,col): return 6
-  #   if check_three(0,0,1,1,2,2): return 7
-  #   if check_three(1,1,2,2,3,3): return 8
-  #   if check_three(2,2,3,3,4,4): return 8
-  #   if check_three(3,3,4,4,5,5): return 8
-  #   if check_three(4,4,5,5,6,6): return 9
-  #   if check_three(0,6,1,5,2,4): return 10
-  #   if check_three(1,5,2,4,3,3): return 11
-  #   if check_three(2,4,3,3,4,2): return 11
-  #   if check_three(3,3,4,2,5,1): return 11
-  #   if check_three(4,2,5,1,6,0): return 12
-  #   return False  
-    
-  #   def check_four(x1,y1,x2,y2,x3,y3,x4,y4):
-  #     return b[x1][y1] == player and b[x2][y2] == player and b[x3][y3] == player and b[x4][y4] == player and b[x5][y5] == player
-  #   for row in range(0,7):
-  #     if check_four(row,0,row,1,row,2,row,3): return True
-  #     if check_four(row,1,row,2,row,3,row,4): return True
-  #     if check_four(row,2,row,3,row,4,row,5): return True
-  #     if check_four(row,3,row,4,row,5,row,6): return True
-  #   for col in range(0,7):
-  #     if check_four(0,col,1,col,2,col,3,col): return True
-  #     if check_four(1,col,2,col,3,col,4,col): return True
-  #     if check_four(2,col,3,col,4,col,5,col): return True
-  #     if check_four(3,col,4,col,5,col,6,col): return True
-  #   if check_four(0,0,1,1,2,2,3,3): return True
-  #   if check_four(1,1,2,2,3,3,4,4): return True
-  #   if check_four(2,2,3,3,4,4,5,5): return True
-  #   if check_four(3,3,4,4,5,5,6,6): return True
-  #   if check_four(0,6,1,5,2,4,3,3): return True
-  #   if check_four(1,5,2,4,3,3,4,2): return True
-  #   if check_four(2,4,3,3,4,2,5,1): return True
-  #   if check_four(3,3,4,2,5,1,6,0): return True
-  #   return False
-
-    # def board_score(self, player):
-    # score = 0
-    # score_four = player*-100
-    # score_three = player*-50
-    # b = self.board
-
-    # #Lower score means player 1 is better, higher score means player 2 is better
-    # #need to create an algorithm that recognizes boards where opponent has many pieces in a row are bad
-    # #and board where algorithm has many points in a row is good
-    # #make a test that looks at 5 squares and does a test to see what is in those 5 squares.
-    # def check_three(x1,y1,x2,y2,x3,y3):
-    #   return b[x1][y1] == player and b[x2][y2] == player and b[x3][y3] == player
-    # for row in range(0,7):
-    #   if check_three(row,0,row,1,row,2): score = score + score_three
-    #   if check_three(row,1,row,2,row,3): score = score + score_three
-    #   if check_three(row,2,row,3,row,4): score = score + score_three
-    #   if check_three(row,3,row,4,row,5): score = score + score_three
-    #   if check_three(row,4,row,5,row,6): score = score + score_three
-    # for col in range(0,7):
-    #   if check_three(0,col,1,col,2,col): score = score + score_three
-    #   if check_three(1,col,2,col,3,col): score = score + score_three
-    #   if check_three(2,col,3,col,4,col): score = score + score_three
-    #   if check_three(3,col,4,col,5,col): score = score + score_three
-    #   if check_three(4,col,5,col,6,col): score = score + score_three
-    # for row in range(0,5):
-    #   for col in range(0,5):
-    #     if check_three(row,col,row+1,col+1,row+2,col+2): score = score + score_three
-    #     if check_three(6-row,col,5-row,col+1,4-row,col+2): score = score + score_three
-
-    # def check_four(x1,y1,x2,y2,x3,y3,x4,y4):
-    #   return b[x1][y1] == player and b[x2][y2] == player and b[x3][y3] == player and b[x4][y4] == player
-    # for row in range(0,7):
-    #   if check_four(row,0,row,1,row,2,row,3): score = score + score_four
-    #   if check_four(row,1,row,2,row,3,row,4): score = score + score_four
-    #   if check_four(row,2,row,3,row,4,row,5): score = score + score_four
-    #   if check_four(row,3,row,4,row,5,row,6): score = score + score_four
-    # for col in range(0,7):
-    #   if check_four(0,col,1,col,2,col,3,col): score = score + score_four
-    #   if check_four(1,col,2,col,3,col,4,col): score = score + score_four
-    #   if check_four(2,col,3,col,4,col,5,col): score = score + score_four
-    #   if check_four(3,col,4,col,5,col,6,col): score = score + score_four
-    
-    # for row in range(0,4):
-    #   for col in range(0,4):
-    #     if check_four(row,col,row+1,col+1,row+2,col+2,row+3,col+3): score = score + score_four
-    #     if check_four(6-row,col,5-row,col+1,4-row,col+2,3-row,col+3): score = score + score_four 
-    # return score
-
-
-      # def scan_five(self,x1,y1,x2,y2,x3,y3,x4,y4,x5,y5):
-  #   b = self.board
-  #   player_pieces = 0
-  #   algorithm_pieces = 0
-    
-  #   if 1 == b[x1][y1]: player_pieces+=1
-  #   if 1 == b[x2][y2]: player_pieces+=1
-  #   if 1 == b[x3][y3]: player_pieces+=1
-  #   if 1 == b[x4][y4]: player_pieces+=1
-  #   if 1 == b[x5][y5]: player_pieces+=1
-
-  #   if -1 == b[x1][y1]: algorithm_pieces+=1
-  #   if -1 == b[x2][y2]: algorithm_pieces+=1
-  #   if -1 == b[x3][y3]: algorithm_pieces+=1
-  #   if -1 == b[x4][y4]: algorithm_pieces+=1
-  #   if -1 == b[x5][y5]: algorithm_pieces+=1
-
-  #   return player_pieces, algorithm_pieces 
-
-
+      #increment counter
+      c=c+0.5
+      #switch player
+      player = -1*player
